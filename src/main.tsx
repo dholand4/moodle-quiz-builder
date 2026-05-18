785928264d3fdfd3f15d42fd23285a9d008e0286
Daniel Holanda <danielholanda55@gmail.com>
2026-05-18 18:37:02 -0300

    feat: redesign color palette from orange to cyan matching logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/tokens.css'
 import App from './pages/Home/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
